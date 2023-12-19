d95054a0d68147c2d98366af36f110076f949b11
bhaskardhage9511 <127721606+bhaskardhage9511@users.noreply.github.com>
2023-12-19 08:02:47 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-project';
+
+  dropdown1 = false;
+  displayDropdown1(){
+    this.dropdown1 = true;
+  }
+  hideDropdown1(){
+    this.dropdown1 = false;
+  }
+
+
+  dropdown2 = false;
+  displayDropdown2(){
+    this.dropdown2 = true;
+  }
+  hideDropdown2(){
+    this.dropdown2 = false;
+  }
+
+  dropdown3 = false;
+  displayDropdown3(){
+    this.dropdown3 = true;
+  }
+  hideDropdown3(){
+    this.dropdown3 = false;
+  }
 }
